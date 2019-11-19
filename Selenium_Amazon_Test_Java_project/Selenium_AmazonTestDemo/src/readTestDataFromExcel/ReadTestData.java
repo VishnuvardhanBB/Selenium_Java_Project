9566e9235b00748c5df128b1e2e991f89b18d1e7
VishnuvardhanBB <56108127+VishnuvardhanBB@users.noreply.github.com>
2019-11-19 20:35:02 +0530

    Changed the source code

--- Selenium_Amazon_Test_Java_project/Selenium_AmazonTestDemo/src/readTestDataFromExcel/ReadTestData.java
+++ Selenium_Amazon_Test_Java_project/Selenium_AmazonTestDemo/src/readTestDataFromExcel/ReadTestData.java
@@ -1,10 +1,15 @@
 package readTestDataFromExcel;
 import java.io.File;
 import java.io.FileInputStream;
+
 //import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 //import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ReadTestData {
 	public static void main (String[] args) throws Exception
@@ -16,15 +21,42 @@ public class ReadTestData {
 		int count = sheet1.getLastRowNum(); // row count from 0 to n-1
 		System.out.println("Total row count:: "+count);
 		String Price= null;
+		String PriceValue=null;
 		String BatteryCategory= null;
 		String InternalMemory= null;
 		String ScreenSize= null;
-		for (int i=0; i<=count; i++)
+
+		System.setProperty("webdriver.gecko.driver","C:\\Vishnuvardhan_Balaji_System_Backup\\Mtech_Software_System\\geckodriver-v0.25.0-win64\\geckodriver.exe");
+		WebDriver driver1= new FirefoxDriver();
+		driver1.get("https://www.amazon.in");
+		
+		
+		
+		WebElement SearchItem = driver1.findElement(By.id("twotabsearchtextbox"));
+		SearchItem.sendKeys("mobiles");
+		WebElement SearchButton = driver1.findElement(By.className("nav-input"));
+	    SearchButton.click();			
+	    driver1.navigate().to("https://www.amazon.in/s?k=mobiles&rh=n%3A976419031%2Cn%3A1805560031&dc&qid=1569511173&rnid=3576079031&ref=sr_nr_n_2");
+	    	    
+		for (int i=1; i<=count; i++)
 		{
-			Price= sheet1.getRow(i).getCell(1).getStringCellValue();
-			BatteryCategory= sheet1.getRow(i).getCell(2).getStringCellValue();
-			InternalMemory= sheet1.getRow(i).getCell(3).getStringCellValue();
-			ScreenSize= sheet1.getRow(i).getCell(4).getStringCellValue();
+			PriceValue=  sheet1.getRow(i).getCell(1).getStringCellValue();
+			Price= sheet1.getRow(i).getCell(2).getStringCellValue();
+			BatteryCategory= sheet1.getRow(i).getCell(3).getStringCellValue();
+			InternalMemory= sheet1.getRow(i).getCell(4).getStringCellValue();
+			ScreenSize= sheet1.getRow(i).getCell(5).getStringCellValue();
+			
+			driver1.navigate().to("https://www.amazon.in/s?k=mobiles&i=electronics&rh=n%3A976419031%2Cn%3A1805560031%2Cp_n_feature_thirteen_browse-bin%3A8561100031%7C8561101031%2Cp_n_feature_six_browse-bin%3A6631756031&dc&qid=1574172563&rnid=1318502031&ref=sr_nr_p_36_2");
+			
+		    WebElement MobileMinPrice = driver1.findElement(By.id("low-price"));
+		    MobileMinPrice.sendKeys("0");
+		    
+		    WebElement MobileMaxPrice = driver1.findElement(By.id("high-price"));
+		    MobileMaxPrice.sendKeys(PriceValue);
+		    
+		    WebElement SearchPriceButton = driver1.findElement(By.className("a-button-input"));
+		    SearchPriceButton.click(); 
+		    System.out.println(PriceValue);
 			System.out.println(Price);
 			System.out.println(BatteryCategory);
 			System.out.println(InternalMemory);
